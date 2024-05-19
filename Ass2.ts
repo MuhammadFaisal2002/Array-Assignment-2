e2e207d792e456137b3779ca60519e7fcc4a32a5
MF Developer <124426958+MuhammadFaisal2002@users.noreply.github.com>
2024-05-19 17:12:59 +0500

    Your commit message

---
+++ Ass2.ts
@@ -0,0 +1,37 @@
+let fruits : string[]= ["Mango", "Apple" ,"Banana", "orange"]
+console.log(fruits);
+let numbers : number[];
+numbers = [10,20,30,40,50]
+console.log(numbers);
+let thirdFruit = fruits[2]
+console.log(thirdFruit);
+numbers[1] = 25
+console.log(numbers);
+fruits.push("Grape")
+console.log(fruits);
+let lastFruit = fruits.pop()
+console.log(fruits);
+console.log(lastFruit);
+let firstFruit = fruits.shift()
+console.log(firstFruit);
+console.log(fruits);
+fruits.unshift("Kiwi")
+console.log(fruits);
+fruits.splice(1,2)
+console.log(fruits);
+fruits.splice(2,0,"pineapple","pear")
+console.log(fruits);
+let citrusFruits = fruits.slice(0,2)
+console.log(citrusFruits);
+console.log(fruits);
+let lastTwoFruit = fruits.slice(2,4)
+console.log(lastTwoFruit);
+
+
+
+
+
+
+
+
+ 
